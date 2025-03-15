253a5500ec20f65ffc90317baee4f6555b11c198
DigitalM0nster <valentin.webs@gmail.com>
2025-03-15 03:17:37 +0300

    update

--- hubarch_react_app/src/components/mainScreen/MainScreen.tsx
+++
@@ -1,6 +0,0 @@
-// import { useNavigate } from "react-router-dom";
-// import styles from "./styles.module.scss";
-
-export default function MainScreen() {
-	return <>Главная</>;
-}
--- hubarch_react_app/src/config.ts
+++
@@ -1,2 +0,0 @@
-const API_URL: string = "http://hubarch.local/wp-json/";
-export default API_URL;
